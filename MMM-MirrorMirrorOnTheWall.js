d44f6a63d01627ddd72885169c9a502cd74dad51
Jeremias Arnstadt <J.Arnstadt@t-systems.com>
2017-10-17 15:11:52 +0200

    removed everthing except module handling

--- MMM-MirrorMirrorOnTheWall.js
+++ MMM-MirrorMirrorOnTheWall.js
@@ -3,7 +3,7 @@
 /* Magic Mirror
  * Module: MMM-MirrorMirrorOnTheWall
  *
- * 
+ *
  * MIT Licensed.
  */
 
@@ -64,37 +64,6 @@ Module.register('MMM-MirrorMirrorOnTheWall', {
   getDom: function() {
     wrapper = document.createElement("div");
     wrapper.className = 'thin large bright';
-
-    if (this.result && !this.clear) {
-      if (this.result.images) {
-        var row = document.createElement("div")
-        row.className = "row"
-
-        for (var i = 0; i < this.result.images.length; i++) {
-          var img = document.createElement("img");
-          img.src = this.result.images[i].url
-          row.appendChild(img)
-        }
-        wrapper.appendChild(row)
-      }
-
-      if (this.result.videoId) {
-        var videoWrapper = document.createElement("div")
-        videoWrapper.className = "videoWrapper"
-        var iframe = document.createElement('iframe')
-        iframe.src = "https://www.youtube.com/embed/" + this.result.videoId + "?autoplay=1&controls=0"
-        videoWrapper.appendChild(iframe)
-        wrapper.appendChild(videoWrapper)
-      }
-
-      if (this.result.displayText) {
-        var h1 = document.createElement('h1')
-        h1.className = "animated fadeIn"
-        var t = document.createTextNode(this.result.displayText)
-        h1.appendChild(t)
-        wrapper.appendChild(h1)
-      }
-    }
 
     return wrapper;
   }
